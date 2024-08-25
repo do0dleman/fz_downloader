30b6e65a5c1764889af6543c91a9137890311a25
do0dleman <97869897+do0dleman@users.noreply.github.com>
2024-08-25 10:16:37 +0300

    made it work with multiple tables on the same page

--- src/content.ts
+++ src/content.ts
@@ -33,27 +33,34 @@ const t = async () => {
   const url = `https://www.fromzero.com/api/course/jfz2/lessons/${urlParams[3]}/sublessons/${urlParams[4]}`
   const resRaw = await axios.get(url)
   const res = JSON.parse(resRaw.request.response) as fzReq
+  console.log(res.si_sublesson.parts)
+  res.si_sublesson.parts.forEach(part => {
+    part.widgets.forEach(widget => {
+      if (!widget.table) {
+        return
+      }
+      widget.table.rows.forEach(t => {
+        console.log('----------------[ New Word ]---------------------')
+        console.log(t[0][0].text)
+        const recordLink = t[0][0].record?.sounds['Kanako']
+        const recordId = t[0][0].record?._id
+        const wordSpan = document.querySelector(`span[data-t="sound-text-${recordId}"]`);
 
-  res.si_sublesson.parts[0].widgets[0].table.rows.forEach(t => {
-    console.log('----------------[ New Word ]---------------------')
-    console.log(t[0][0].text)
-    const recordLink = t[0][0].record?.sounds['Kanako']
-    const recordId = t[0][0].record?._id
-    const wordSpan = document.querySelector(`span[data-t="sound-text-${recordId}"]`);
+        const downloadContainer = wordSpan?.parentNode?.parentNode?.parentNode?.parentNode;
+        if (downloadContainer?.lastChild?.nodeName === "A") {
+          return
+        }
+        const link = document.createElement("a")
+        link.classList.add("downloadButton")
 
-    const downloadContainer = wordSpan?.parentNode?.parentNode?.parentNode?.parentNode;
-    if (downloadContainer?.lastChild?.nodeName === "A") {
-      return
-    }
-    const link = document.createElement("a")
-    link.classList.add("downloadButton")
+        const i = document.createElement("i")
+        link.appendChild(i)
+        link.href = recordLink ?? ""
+        i.classList.add(...["play", "circular", "icon", "no-select", "FZPlayButton"])
 
-    const i = document.createElement("i")
-    link.appendChild(i)
-    link.href = recordLink ?? ""
-    i.classList.add(...["play", "circular", "icon", "no-select", "FZPlayButton"])
-
-    downloadContainer?.appendChild(link)
+        downloadContainer?.appendChild(link)
+      })
+    })
   })
 }
 t()
